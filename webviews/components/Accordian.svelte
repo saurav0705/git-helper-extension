18b42d598a78f076470bbbf6043144fcfed51f40
Saurav Aggarwal <saurav.aggarwal@flipkart.com>
2021-03-13 13:09:58 +0530

    implemented search

--- webviews/components/Accordian.svelte
+++ webviews/components/Accordian.svelte
@@ -3,15 +3,37 @@
     export let title;
     export let list;
     export let type;
+    let showList = [];
     let show = true;
+    let searchText = '';
+
+    const filterList = () => {
+        showList = list.filter((item) => item.branch.includes(searchText));
+    };
+    $: {
+        if (searchText || true) {
+            filterList();
+        }
+    }
 </script>
 
 <div>
     <div on:click={() => (show = !show)}>{title} ></div>
     <div class={show ? 'content show' : 'content hide'}>
-        {#each list as item, index}
-            <Tile selected={item.selected} title={item.branch} type={type}/>
-        {/each}
+        <input
+            type="search"
+            bind:value={searchText}
+            placeholder="Search Branch"
+        />
+        {#if showList.length > 0 || searchText.length}
+            {#each showList as item, index}
+                <Tile selected={item.selected} title={item.branch} {type} />
+            {/each}
+        {:else}
+            {#each list as item, index}
+                <Tile selected={item.selected} title={item.branch} {type} />
+            {/each}
+        {/if}
     </div>
 </div>
 
